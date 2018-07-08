a84c090a694a5fdaa88010c82cd72202f5ecd0fd
emanuelltcarvalho <emanuelltcarvalho@users.noreply.github.com>
2018-07-08 17:43:28 -0300

    Splash screen

--- app/src/main/java/br/trabalho221/odisseia/Splash.java
+++ app/src/main/java/br/trabalho221/odisseia/Splash.java
@@ -1,7 +1,6 @@
 package br.trabalho221.odisseia;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends Activity {
