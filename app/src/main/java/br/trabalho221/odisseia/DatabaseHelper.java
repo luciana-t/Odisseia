6a915d43d6824701d1e8a84fdb1ad4069556b27e
luciana-t <luciana.tavares@ufv.com>
2018-07-07 22:32:19 -0300

    Fim do tutorial 3, banco de dados funcionando para teste

--- app/src/main/java/br/trabalho221/odisseia/DatabaseHelper.java
+++ app/src/main/java/br/trabalho221/odisseia/DatabaseHelper.java
@@ -1,21 +1,39 @@
 package br.trabalho221.odisseia;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public static final String DATABASE_NAME = "DBOdisseia.db";
+    public static final String TABLE_NAME = "Odisseia_table";
+    public static final String ID = "ID";
+    public static final String NAME = "NAME";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
+    }
+    public boolean insertData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(NAME, name);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) return false;
+        else return true;
     }
 }
