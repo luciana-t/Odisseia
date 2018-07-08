680934f1aa8fb24239494f4f611c6d7bc29bfc69
emanuelltcarvalho <emanuelltcarvalho@users.noreply.github.com>
2018-07-08 17:16:59 -0300

    Splash screen

---
+++ app/src/main/java/br/trabalho221/odisseia/Splash.java
@@ -0,0 +1,14 @@
+package br.trabalho221.odisseia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
+
